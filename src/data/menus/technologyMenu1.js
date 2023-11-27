440d714958d50a1a6b7df304fb9c870d346c0e67
sisogame <adlove95@gmail.com>
2023-11-27 15:05:42 +0900

    공통 커밋

---
+++ src/data/menus/technologyMenu1.js
@@ -0,0 +1,13 @@
+export const technologyMenuItems = [
+	{
+		name: "Supporting Standards",
+		link: "/technology/item1",
+		level2: [
+			{ name: "DAB Solution"},
+			{ name: "CDR Solution"},
+			{ name: "DRM Solution"},
+			{ name: "DRHD Radio"},
+
+		],
+	},
+];
---
+++ src/data/menus/technologyMenu2.js
@@ -0,0 +1,8 @@
+export const technologyMenuItems = [
+	{
+		name: "Audio Solution",
+	},
+	{
+		name: "All happens in the SoC",
+	},
+];
