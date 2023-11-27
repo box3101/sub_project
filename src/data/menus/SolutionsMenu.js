d259a755459d6f84ed2883a34868abaa2168ec77
cylee_ispark <tony.at.cp@gmail.com>
2023-11-27 14:35:12 +0900

    13

---
+++ src/data/menus/SolutionsMenu.js
@@ -0,0 +1,26 @@
+export const technologyMenuItems = [
+	{
+		name: "Supporting Standards",
+		link: "/technology/item1",
+		level2: [
+			{ name: "DAB Solution1", link: "" },
+			{ name: "DAB Solution2", link: "" },
+			{ name: "DAB Solution3", link: "" },
+			// { name: "DAB Solution3", link: "" },
+		],
+	},
+	{
+		name: "Operating System(OS)",
+		link: "/technology/item2",
+		level2: [
+			{ name: "DAB Solution4", link: "" },
+		],
+	},
+	{
+		name: "Supporting SoCs",
+		link: "/technology/item2",
+		level2: [
+			{ name: "DAB Solution5", link: "" },
+		],
+	},
+];
