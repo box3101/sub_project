a9ee17eeafde2c213f494a608fc6270f5cf6e6f0
youxx329 <younggu329@naver.com>
2023-12-04 17:57:50 +0900

    탭기능 스크립트 수정 완료, 모바일 작업 한 것까지 커밋

--- public/js/SolutionsTab.js
+++ public/js/SolutionsTab.js
@@ -1,26 +1,25 @@
 const tabBtn = document.querySelectorAll(".tab-button");
 const tabCnt = document.querySelectorAll(".tab-content-wrap");
 const tabList = document.querySelector(".tab-list");
-const tab = document.querySelector(".solu-menu09");
 
+// 탭 클릭시 클릭 이벤트 호출
 tabList.addEventListener("click", function (e) {
 	for (let i = 0; i < tabBtn.length; i++) {
 		if (e.target == tabBtn[i]) {
-			tabClickEvent(i);
+			tabClickEvent(e);
 		}
 	}
 });
 
-function tabClickEvent(event) {
+function tabClickEvent(e) {
+	let current = e.target;
 	for (let i = 0; i < tabBtn.length; i++) {
-		tabBtn[i].addEventListener("click", function () {
-			for (let j = 0; j < tabBtn.length; j++) {
-				tabBtn[j].classList.remove("on");
-				tabCnt[j].classList.remove("show");
-			}
-
-			tabBtn[i].classList.add("on");
-			tabCnt[i].classList.add("show");
-		});
+		// 모든 탭의 on, show 제거
+		tabBtn[i].classList.remove("on");
+		tabCnt[i].classList.remove("show");
 	}
+	// 현재 클릭한 탭에 on 추가
+	current.classList.add("on");
+	// 현재 클릭한 탭의 data 값을 이용하여 show 추가
+	tabCnt[current.dataset.id].classList.add("show");
 }
